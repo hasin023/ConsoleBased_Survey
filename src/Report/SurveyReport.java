d8cbb3d7a0eaefc4ea7d14d2b2b3fd119fe734c8
Nahiyan <nahiyan49@gmail.com>
2024-01-08 01:52:05 +0600

    Fixed survey respondent and survey report

--- src/Report/SurveyReport.java
+++ src/Report/SurveyReport.java
@@ -1,55 +1,21 @@
 package Report;
 
-import Survey.Question;
-import Survey.Survey;
-import User.User;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class SurveyReport {
-    private final Survey survey;
-    private final List<Question> questions;
-    private final Map<User, List<String>> userResponses;
+    private String respondentUsername;
+    private Map<Integer, String> responses;
 
-    public SurveyReport(Survey survey, List<Question> questions, List<User> respondents) {
-        this.survey = survey;
-        this.questions = questions;
-        userResponses = new HashMap<>();
-        for (User user : respondents) {
-            userResponses.put(user, new ArrayList<>());
-        }
+    public SurveyReport(String respondentUsername, Map<Integer, String> responses) {
+        this.respondentUsername = respondentUsername;
+        this.responses = responses;
     }
 
-    public Survey getSurvey() {
-        return survey;
+    public String getRespondentUsername() {
+        return respondentUsername;
     }
 
-    public List<Question> getQuestions() {
-        return questions;
-    }
-
-    public Map<User, List<String>> getUserResponses() {
-        return userResponses;
-    }
-
-    public void addUserResponse(User user, List<String> responses) {
-        userResponses.get(user).addAll(responses);
-    }
-
-    public List<String> getQuestionResponses(Question question) {
-        List<String> responses = new ArrayList<>();
-        for (User user : userResponses.keySet()) {
-            List<String> userResponse = userResponses.get(user);
-            int questionIndex = questions.indexOf(question);
-            if (questionIndex >= 0 && questionIndex < userResponse.size()) {
-                responses.add(userResponse.get(questionIndex));
-            } else {
-                responses.add("No response");
-            }
-        }
+    public Map<Integer, String> getResponses() {
         return responses;
     }
 }
--- src/User/SurveyRespondent.java
+++ src/User/SurveyRespondent.java
@@ -1,11 +1,9 @@
 package User;
 
-import Report.SurveyReport;
 import Survey.Survey;
 import TextFileHelper.ReportTextFileHandler;
 
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 public class SurveyRespondent extends User{
@@ -29,31 +27,4 @@ public class SurveyRespondent extends User{
         return completedSurveys.contains(survey);
     }
 
-    public void completeSurvey(Survey survey, List<String> responses) {
-        if (!completedSurveys.contains(survey)) {
-            survey.close();
-            completedSurveys.add(survey);
-            SurveyReport surveyReport = new SurveyReport(survey, survey.getQuestions(), List.of(this));
-            surveyReport.addUserResponse(this, responses);
-            reportTextFileHandler.saveSurveyReport(surveyReport);
-        }
-    }
-
-    public void viewSurveyStatistics(Survey survey) {
-        if (completedSurveys.contains(survey)) {
-            SurveyReport surveyReport = reportTextFileHandler.loadSurveyReport(survey);
-            System.out.println("Survey Title: " + surveyReport.getSurvey().getTitle());
-            System.out.println("Number of Responses: " + surveyReport.getUserResponses().size());
-            System.out.println("Questions:");
-            for (int i = 0; i < surveyReport.getQuestions().size(); i++) {
-                System.out.println("Question " + (i + 1) + ": " + surveyReport.getQuestions().get(i).getQuestion());
-                System.out.println("Responses:");
-                for (String response : surveyReport.getQuestionResponses(surveyReport.getQuestions().get(i))) {
-                    System.out.println(response);
-                }
-            }
-        } else {
-            System.out.println("You have not completed this survey.");
-        }
-    }
 }
